d6b781848453854916a2104ab07478c6c3aefe14
Batyeste <batystebatybat@gmail.com>
2026-05-27 13:53:05 +0200

    Ajout de la fonctionnalité d'importation des ports mondiaux depuis le WPI, création modèles et des routes nécessaires pour les ports

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Port(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100, blank=True)
+    region = models.CharField(max_length=100, blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    wpi_number = models.CharField(max_length=20, blank=True, null=True, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
