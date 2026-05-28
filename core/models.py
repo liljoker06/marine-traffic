bf526de2bb7e568742505db0f7e54434914f9042
Batyeste <batystebatybat@gmail.com>
2026-05-28 11:48:56 +0200

    ajout de la taille des ports en bdd et à l'affichage. - trie sur la taille des ports dans /ports - stockage en cache sur la partie maps - système de zoom/dézoom qui affiche les grands et/ou petit  ports

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Port(models.Model):
     name = models.CharField(max_length=200)
     country = models.CharField(max_length=100, blank=True)
@@ -8,9 +8,16 @@ class Port(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     wpi_number = models.CharField(max_length=20, blank=True, null=True, unique=True)
-
+    harbor_size = models.CharField(
+        max_length=1, blank=True, default='',
+        help_text='L=Large, M=Medium, S=Small, V=Very Small',
+        db_index=True,
+    )
     class Meta:
         ordering = ['name']
+        indexes = [
+            models.Index(fields=['latitude', 'longitude'], name='port_latlon_idx'),
+        ]
 
     def __str__(self):
         return f"{self.name} ({self.country})"
